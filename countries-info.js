6ca2f54382236850257147f858bd3c461b4d5bbf
Iworker Developer <109700998+iworkerdev@users.noreply.github.com>
2023-12-14 07:31:54 +0300

    Add Countries Info.Js file

---
+++ countries-info.js
@@ -0,0 +1,1102 @@
+var countries_flags_and_codes = [
+  {
+    name: 'Afghanistan',
+    phoneCode: '93',
+    flagClassCode: 'af',
+  },
+  {
+    name: 'Albania',
+    phoneCode: '355',
+    flagClassCode: 'al',
+  },
+  {
+    name: 'Algeria',
+    phoneCode: '213',
+    flagClassCode: 'dz',
+  },
+  {
+    name: 'American Samoa',
+    phoneCode: '1',
+    flagClassCode: 'as',
+  },
+  {
+    name: 'Andorra',
+    phoneCode: '376',
+    flagClassCode: 'ad',
+  },
+  {
+    name: 'Angola',
+    phoneCode: '244',
+    flagClassCode: 'ao',
+  },
+  {
+    name: 'Anguilla',
+    phoneCode: '1',
+    flagClassCode: 'ai',
+  },
+  {
+    name: 'Antigua and Barbuda',
+    phoneCode: '1',
+    flagClassCode: 'ag',
+  },
+  {
+    name: 'Argentina',
+    phoneCode: '54',
+    flagClassCode: 'ar',
+  },
+  {
+    name: 'Armenia',
+    phoneCode: '374',
+    flagClassCode: 'am',
+  },
+  {
+    name: 'Aruba',
+    phoneCode: '297',
+    flagClassCode: 'aw',
+  },
+  {
+    name: 'Australia',
+    phoneCode: '61',
+    flagClassCode: 'au',
+  },
+  {
+    name: 'Austria',
+    phoneCode: '43',
+    flagClassCode: 'at',
+  },
+  {
+    name: 'Azerbaijan',
+    phoneCode: '994',
+    flagClassCode: 'az',
+  },
+  {
+    name: 'Bahamas',
+    phoneCode: '1',
+    flagClassCode: 'bs',
+  },
+  {
+    name: 'Bahrain',
+    phoneCode: '973',
+    flagClassCode: 'bh',
+  },
+  {
+    name: 'Bangladesh',
+    phoneCode: '880',
+    flagClassCode: 'bd',
+  },
+  {
+    name: 'Barbados',
+    phoneCode: '1',
+    flagClassCode: 'bb',
+  },
+  {
+    name: 'Belarus',
+    phoneCode: '375',
+    flagClassCode: 'by',
+  },
+  {
+    name: 'Belgium',
+    phoneCode: '32',
+    flagClassCode: 'be',
+  },
+  {
+    name: 'Belize',
+    phoneCode: '501',
+    flagClassCode: 'bz',
+  },
+  {
+    name: 'Benin',
+    phoneCode: '229',
+    flagClassCode: 'bj',
+  },
+  {
+    name: 'Bermuda',
+    phoneCode: '1',
+    flagClassCode: 'bm',
+  },
+  {
+    name: 'Bhutan',
+    phoneCode: '975',
+    flagClassCode: 'bt',
+  },
+  {
+    name: 'Bolivia',
+    phoneCode: '591',
+    flagClassCode: 'bo',
+  },
+  {
+    name: 'Bosnia and Herzegovina',
+    phoneCode: '387',
+    flagClassCode: 'ba',
+  },
+  {
+    name: 'Botswana',
+    phoneCode: '267',
+    flagClassCode: 'bw',
+  },
+  {
+    name: 'Brazil',
+    phoneCode: '55',
+    flagClassCode: 'br',
+  },
+  {
+    name: 'British Indian Ocean Territory',
+    phoneCode: '246',
+    flagClassCode: 'io',
+  },
+  {
+    name: 'British Virgin Islands',
+    phoneCode: '1',
+    flagClassCode: 'vg',
+  },
+  {
+    name: 'Brunei',
+    phoneCode: '673',
+    flagClassCode: 'bn',
+  },
+  {
+    name: 'Bulgaria',
+    phoneCode: '359',
+    flagClassCode: 'bg',
+  },
+  {
+    name: 'Burkina Faso',
+    phoneCode: '226',
+    flagClassCode: 'bf',
+  },
+  {
+    name: 'Burundi',
+    phoneCode: '257',
+    flagClassCode: 'bi',
+  },
+  {
+    name: 'Cambodia',
+    phoneCode: '855',
+    flagClassCode: 'kh',
+  },
+  {
+    name: 'Cameroon',
+    phoneCode: '237',
+    flagClassCode: 'cm',
+  },
+  {
+    name: 'Canada',
+    phoneCode: '1',
+    flagClassCode: 'ca',
+  },
+  {
+    name: 'Cape Verde',
+    phoneCode: '238',
+    flagClassCode: 'cv',
+  },
+  {
+    name: 'Caribbean Netherlands',
+    phoneCode: '599',
+    flagClassCode: 'bq',
+  },
+  {
+    name: 'Cayman Islands',
+    phoneCode: '1',
+    flagClassCode: 'ky',
+  },
+  {
+    name: 'Central African Republic',
+    phoneCode: '236',
+    flagClassCode: 'cf',
+  },
+  {
+    name: 'Chad',
+    phoneCode: '235',
+    flagClassCode: 'td',
+  },
+  {
+    name: 'Chile',
+    phoneCode: '56',
+    flagClassCode: 'cl',
+  },
+  {
+    name: 'China',
+    phoneCode: '86',
+    flagClassCode: 'cn',
+  },
+  {
+    name: 'Christmas Island',
+    phoneCode: '61',
+    flagClassCode: 'cx',
+  },
+  {
+    name: 'Cocos (Keeling) Islands',
+    phoneCode: '61',
+    flagClassCode: 'cc',
+  },
+  {
+    name: 'Colombia',
+    phoneCode: '57',
+    flagClassCode: 'co',
+  },
+  {
+    name: 'Comoros',
+    phoneCode: '269',
+    flagClassCode: 'km',
+  },
+  {
+    name: 'Congo (DRC)',
+    phoneCode: '243',
+    flagClassCode: 'cd',
+  },
+  {
+    name: 'Congo (Republic)',
+    phoneCode: '242',
+    flagClassCode: 'cg',
+  },
+  {
+    name: 'Cook Islands',
+    phoneCode: '682',
+    flagClassCode: 'ck',
+  },
+  {
+    name: 'Costa Rica',
+    phoneCode: '506',
+    flagClassCode: 'cr',
+  },
+  {
+    name: 'Côte d’Ivoire',
+    phoneCode: '225',
+    flagClassCode: 'ci',
+  },
+  {
+    name: 'Croatia',
+    phoneCode: '385',
+    flagClassCode: 'hr',
+  },
+  {
+    name: 'Cuba',
+    phoneCode: '53',
+    flagClassCode: 'cu',
+  },
+  {
+    name: 'Curaçao',
+    phoneCode: '599',
+    flagClassCode: 'cw',
+  },
+  {
+    name: 'Cyprus',
+    phoneCode: '357',
+    flagClassCode: 'cy',
+  },
+  {
+    name: 'Czech Republic',
+    phoneCode: '420',
+    flagClassCode: 'cz',
+  },
+  {
+    name: 'Denmark',
+    phoneCode: '45',
+    flagClassCode: 'dk',
+  },
+  {
+    name: 'Djibouti',
+    phoneCode: '253',
+    flagClassCode: 'dj',
+  },
+  {
+    name: 'Dominica',
+    phoneCode: '1',
+    flagClassCode: 'dm',
+  },
+  {
+    name: 'Dominican Republic',
+    phoneCode: '1',
+    flagClassCode: 'do',
+  },
+  {
+    name: 'Ecuador',
+    phoneCode: '593',
+    flagClassCode: 'ec',
+  },
+  {
+    name: 'Egypt',
+    phoneCode: '20',
+    flagClassCode: 'eg',
+  },
+  {
+    name: 'El Salvador',
+    phoneCode: '503',
+    flagClassCode: 'sv',
+  },
+  {
+    name: 'Equatorial Guinea',
+    phoneCode: '240',
+    flagClassCode: 'gq',
+  },
+  {
+    name: 'Eritrea',
+    phoneCode: '291',
+    flagClassCode: 'er',
+  },
+  {
+    name: 'Estonia',
+    phoneCode: '372',
+    flagClassCode: 'ee',
+  },
+  {
+    name: 'Ethiopia',
+    phoneCode: '251',
+    flagClassCode: 'et',
+  },
+  {
+    name: 'Falkland Islands',
+    phoneCode: '500',
+    flagClassCode: 'fk',
+  },
+  {
+    name: 'Faroe Islands',
+    phoneCode: '298',
+    flagClassCode: 'fo',
+  },
+  {
+    name: 'Fiji',
+    phoneCode: '679',
+    flagClassCode: 'fj',
+  },
+  {
+    name: 'Finland',
+    phoneCode: '358',
+    flagClassCode: 'fi',
+  },
+  {
+    name: 'France',
+    phoneCode: '33',
+    flagClassCode: 'fr',
+  },
+  {
+    name: 'French Guiana',
+    phoneCode: '594',
+    flagClassCode: 'gf',
+  },
+  {
+    name: 'French Polynesia',
+    phoneCode: '689',
+    flagClassCode: 'pf',
+  },
+  {
+    name: 'Gabon',
+    phoneCode: '241',
+    flagClassCode: 'ga',
+  },
+  {
+    name: 'Gambia',
+    phoneCode: '220',
+    flagClassCode: 'gm',
+  },
+  {
+    name: 'Georgia',
+    phoneCode: '995',
+    flagClassCode: 'ge',
+  },
+  {
+    name: 'Germany',
+    phoneCode: '49',
+    flagClassCode: 'de',
+  },
+  {
+    name: 'Ghana',
+    phoneCode: '233',
+    flagClassCode: 'gh',
+  },
+  {
+    name: 'Gibraltar',
+    phoneCode: '350',
+    flagClassCode: 'gi',
+  },
+  {
+    name: 'Greece',
+    phoneCode: '30',
+    flagClassCode: 'gr',
+  },
+  {
+    name: 'Greenland',
+    phoneCode: '299',
+    flagClassCode: 'gl',
+  },
+  {
+    name: 'Grenada',
+    phoneCode: '1',
+    flagClassCode: 'gd',
+  },
+  {
+    name: 'Guadeloupe',
+    phoneCode: '590',
+    flagClassCode: 'gp',
+  },
+  {
+    name: 'Guam',
+    phoneCode: '1',
+    flagClassCode: 'gu',
+  },
+  {
+    name: 'Guatemala',
+    phoneCode: '502',
+    flagClassCode: 'gt',
+  },
+  {
+    name: 'Guernsey',
+    phoneCode: '44',
+    flagClassCode: 'gg',
+  },
+  {
+    name: 'Guinea',
+    phoneCode: '224',
+    flagClassCode: 'gn',
+  },
+  {
+    name: 'Guinea-Bissau',
+    phoneCode: '245',
+    flagClassCode: 'gw',
+  },
+  {
+    name: 'Guyana',
+    phoneCode: '592',
+    flagClassCode: 'gy',
+  },
+  {
+    name: 'Haiti',
+    phoneCode: '509',
+    flagClassCode: 'ht',
+  },
+  {
+    name: 'Honduras',
+    phoneCode: '504',
+    flagClassCode: 'hn',
+  },
+  {
+    name: 'Hong Kong',
+    phoneCode: '852',
+    flagClassCode: 'hk',
+  },
+  {
+    name: 'Hungary',
+    phoneCode: '36',
+    flagClassCode: 'hu',
+  },
+  {
+    name: 'Iceland',
+    phoneCode: '354',
+    flagClassCode: 'is',
+  },
+  {
+    name: 'India',
+    phoneCode: '91',
+    flagClassCode: 'in',
+  },
+  {
+    name: 'Indonesia',
+    phoneCode: '62',
+    flagClassCode: 'id',
+  },
+  {
+    name: 'Iran',
+    phoneCode: '98',
+    flagClassCode: 'ir',
+  },
+  {
+    name: 'Iraq',
+    phoneCode: '964',
+    flagClassCode: 'iq',
+  },
+  {
+    name: 'Ireland',
+    phoneCode: '353',
+    flagClassCode: 'ie',
+  },
+  {
+    name: 'Israel',
+    phoneCode: '972',
+    flagClassCode: 'il',
+  },
+  {
+    name: 'Italy',
+    phoneCode: '39',
+    flagClassCode: 'it',
+  },
+  {
+    name: 'Jamaica',
+    phoneCode: '1-876',
+    flagClassCode: 'jm',
+  },
+  {
+    name: 'Japan',
+    phoneCode: '81',
+    flagClassCode: 'jp',
+  },
+  {
+    name: 'Jordan',
+    phoneCode: '962',
+    flagClassCode: 'jo',
+  },
+  {
+    name: 'Kazakhstan',
+    phoneCode: '7',
+    flagClassCode: 'kz',
+  },
+  {
+    name: 'Kenya',
+    phoneCode: '254',
+    flagClassCode: 'ke',
+  },
+  {
+    name: 'Kiribati',
+    phoneCode: '686',
+    flagClassCode: 'ki',
+  },
+  {
+    name: 'Kuwait',
+    phoneCode: '965',
+    flagClassCode: 'kw',
+  },
+  {
+    name: 'Kyrgyzstan',
+    phoneCode: '996',
+    flagClassCode: 'kg',
+  },
+  {
+    name: 'Laos',
+    phoneCode: '856',
+    flagClassCode: 'la',
+  },
+  {
+    name: 'Latvia',
+    phoneCode: '371',
+    flagClassCode: 'lv',
+  },
+  {
+    name: 'Lebanon',
+    phoneCode: '961',
+    flagClassCode: 'lb',
+  },
+  {
+    name: 'Lesotho',
+    phoneCode: '266',
+    flagClassCode: 'ls',
+  },
+  {
+    name: 'Liberia',
+    phoneCode: '231',
+    flagClassCode: 'lr',
+  },
+  {
+    name: 'Libya',
+    phoneCode: '218',
+    flagClassCode: 'ly',
+  },
+  {
+    name: 'Liechtenstein',
+    phoneCode: '423',
+    flagClassCode: 'li',
+  },
+  {
+    name: 'Lithuania',
+    phoneCode: '370',
+    flagClassCode: 'lt',
+  },
+  {
+    name: 'Luxembourg',
+    phoneCode: '352',
+    flagClassCode: 'lu',
+  },
+  {
+    name: 'Macau',
+    phoneCode: '853',
+    flagClassCode: 'mo',
+  },
+  {
+    name: 'Macedonia',
+    phoneCode: '389',
+    flagClassCode: 'mk',
+  },
+  {
+    name: 'Madagascar',
+    phoneCode: '261',
+    flagClassCode: 'mg',
+  },
+  {
+    name: 'Malawi',
+    phoneCode: '265',
+    flagClassCode: 'mw',
+  },
+  {
+    name: 'Malaysia',
+    phoneCode: '60',
+    flagClassCode: 'my',
+  },
+  {
+    name: 'Maldives',
+    phoneCode: '960',
+    flagClassCode: 'mv',
+  },
+  {
+    name: 'Mali',
+    phoneCode: '223',
+    flagClassCode: 'ml',
+  },
+  {
+    name: 'Malta',
+    phoneCode: '356',
+    flagClassCode: 'mt',
+  },
+  {
+    name: 'Marshall Islands',
+    phoneCode: '692',
+    flagClassCode: 'mh',
+  },
+  {
+    name: 'Mauritania',
+    phoneCode: '222',
+    flagClassCode: 'mr',
+  },
+  {
+    name: 'Mauritius',
+    phoneCode: '230',
+    flagClassCode: 'mu',
+  },
+  {
+    name: 'Mexico',
+    phoneCode: '52',
+    flagClassCode: 'mx',
+  },
+  {
+    name: 'Micronesia',
+    phoneCode: '691',
+    flagClassCode: 'fm',
+  },
+  {
+    name: 'Moldova',
+    phoneCode: '373',
+    flagClassCode: 'md',
+  },
+  {
+    name: 'Monaco',
+    phoneCode: '377',
+    flagClassCode: 'mc',
+  },
+  {
+    name: 'Mongolia',
+    phoneCode: '976',
+    flagClassCode: 'mn',
+  },
+  {
+    name: 'Montenegro',
+    phoneCode: '382',
+    flagClassCode: 'me',
+  },
+  {
+    name: 'Morocco',
+    phoneCode: '212',
+    flagClassCode: 'ma',
+  },
+  {
+    name: 'Mozambique',
+    phoneCode: '258',
+    flagClassCode: 'mz',
+  },
+  {
+    name: 'Myanmar',
+    phoneCode: '95',
+    flagClassCode: 'mm',
+  },
+  {
+    name: 'Namibia',
+    phoneCode: '264',
+    flagClassCode: 'na',
+  },
+  {
+    name: 'Nauru',
+    phoneCode: '674',
+    flagClassCode: 'nr',
+  },
+  {
+    name: 'Nepal',
+    phoneCode: '977',
+    flagClassCode: 'np',
+  },
+  {
+    name: 'Netherlands',
+    phoneCode: '31',
+    flagClassCode: 'nl',
+  },
+  {
+    name: 'New Zealand',
+    phoneCode: '64',
+    flagClassCode: 'nz',
+  },
+  {
+    name: 'Nicaragua',
+    phoneCode: '505',
+    flagClassCode: 'ni',
+  },
+  {
+    name: 'Niger',
+    phoneCode: '227',
+    flagClassCode: 'ne',
+  },
+  {
+    name: 'Nigeria',
+    phoneCode: '234',
+    flagClassCode: 'ng',
+  },
+  {
+    name: 'North Korea',
+    phoneCode: '850',
+    flagClassCode: 'kp',
+  },
+  {
+    name: 'Norway',
+    phoneCode: '47',
+    flagClassCode: 'no',
+  },
+  {
+    name: 'Oman',
+    phoneCode: '968',
+    flagClassCode: 'om',
+  },
+  {
+    name: 'Pakistan',
+    phoneCode: '92',
+    flagClassCode: 'pk',
+  },
+  {
+    name: 'Palau',
+    phoneCode: '680',
+    flagClassCode: 'pw',
+  },
+  {
+    name: 'Palestine',
+    phoneCode: '970',
+    flagClassCode: 'ps',
+  },
+  {
+    name: 'Panama',
+    phoneCode: '507',
+    flagClassCode: 'pa',
+  },
+  {
+    name: 'Papua New Guinea',
+    phoneCode: '675',
+    flagClassCode: 'pg',
+  },
+  {
+    name: 'Paraguay',
+    phoneCode: '595',
+    flagClassCode: 'py',
+  },
+  {
+    name: 'Peru',
+    phoneCode: '51',
+    flagClassCode: 'pe',
+  },
+  {
+    name: 'Philippines',
+    phoneCode: '63',
+    flagClassCode: 'ph',
+  },
+  {
+    name: 'Poland',
+    phoneCode: '48',
+    flagClassCode: 'pl',
+  },
+  {
+    name: 'Portugal',
+    phoneCode: '351',
+    flagClassCode: 'pt',
+  },
+  {
+    name: 'Puerto Rico',
+    phoneCode: '1-787',
+    flagClassCode: 'pr',
+  },
+  {
+    name: 'Qatar',
+    phoneCode: '974',
+    flagClassCode: 'qa',
+  },
+  {
+    name: 'Romania',
+    phoneCode: '40',
+    flagClassCode: 'ro',
+  },
+  {
+    name: 'Russia',
+    phoneCode: '7',
+    flagClassCode: 'ru',
+  },
+  {
+    name: 'Rwanda',
+    phoneCode: '250',
+    flagClassCode: 'rw',
+  },
+  {
+    name: 'Saint Kitts and Nevis',
+    phoneCode: '1-869',
+    flagClassCode: 'kn',
+  },
+  {
+    name: 'Saint Lucia',
+    phoneCode: '1-758',
+    flagClassCode: 'lc',
+  },
+  {
+    name: 'Saint Vincent and the Grenadines',
+    phoneCode: '1-784',
+    flagClassCode: 'vc',
+  },
+  {
+    name: 'Samoa',
+    phoneCode: '685',
+    flagClassCode: 'ws',
+  },
+  {
+    name: 'San Marino',
+    phoneCode: '378',
+    flagClassCode: 'sm',
+  },
+  {
+    name: 'Sao Tome and Principe',
+    phoneCode: '239',
+    flagClassCode: 'st',
+  },
+  {
+    name: 'Saudi Arabia',
+    phoneCode: '966',
+    flagClassCode: 'sa',
+  },
+  {
+    name: 'Senegal',
+    phoneCode: '221',
+    flagClassCode: 'sn',
+  },
+  {
+    name: 'Serbia',
+    phoneCode: '381',
+    flagClassCode: 'rs',
+  },
+  {
+    name: 'Seychelles',
+    phoneCode: '248',
+    flagClassCode: 'sc',
+  },
+  {
+    name: 'Sierra Leone',
+    phoneCode: '232',
+    flagClassCode: 'sl',
+  },
+  {
+    name: 'Singapore',
+    phoneCode: '65',
+    flagClassCode: 'sg',
+  },
+  {
+    name: 'Slovakia',
+    phoneCode: '421',
+    flagClassCode: 'sk',
+  },
+  {
+    name: 'Slovenia',
+    phoneCode: '386',
+    flagClassCode: 'si',
+  },
+  {
+    name: 'Solomon Islands',
+    phoneCode: '677',
+    flagClassCode: 'sb',
+  },
+  {
+    name: 'Somalia',
+    phoneCode: '252',
+    flagClassCode: 'so',
+  },
+  {
+    name: 'South Africa',
+    phoneCode: '27',
+    flagClassCode: 'za',
+  },
+  {
+    name: 'South Korea',
+    phoneCode: '82',
+    flagClassCode: 'kr',
+  },
+  {
+    name: 'South Sudan',
+    phoneCode: '211',
+    flagClassCode: 'ss',
+  },
+  {
+    name: 'Spain',
+    phoneCode: '34',
+    flagClassCode: 'es',
+  },
+  {
+    name: 'Sri Lanka',
+    phoneCode: '94',
+    flagClassCode: 'lk',
+  },
+  {
+    name: 'Sudan',
+    phoneCode: '249',
+    flagClassCode: 'sd',
+  },
+  {
+    name: 'Suriname',
+    phoneCode: '597',
+    flagClassCode: 'sr',
+  },
+  {
+    name: 'Swaziland',
+    phoneCode: '268',
+    flagClassCode: 'sz',
+  },
+  {
+    name: 'Sweden',
+    phoneCode: '46',
+    flagClassCode: 'se',
+  },
+  {
+    name: 'Switzerland',
+    phoneCode: '41',
+    flagClassCode: 'ch',
+  },
+  {
+    name: 'Syria',
+    phoneCode: '963',
+    flagClassCode: 'sy',
+  },
+  {
+    name: 'Taiwan',
+    phoneCode: '886',
+    flagClassCode: 'tw',
+  },
+  {
+    name: 'Tajikistan',
+    phoneCode: '992',
+    flagClassCode: 'tj',
+  },
+  {
+    name: 'Tanzania',
+    phoneCode: '255',
+    flagClassCode: 'tz',
+  },
+  {
+    name: 'Thailand',
+    phoneCode: '66',
+    flagClassCode: 'th',
+  },
+  {
+    name: 'Togo',
+    phoneCode: '228',
+    flagClassCode: 'tg',
+  },
+  {
+    name: 'Tonga',
+    phoneCode: '676',
+    flagClassCode: 'to',
+  },
+  {
+    name: 'Trinidad and Tobago',
+    phoneCode: '1-868',
+    flagClassCode: 'tt',
+  },
+  {
+    name: 'Tunisia',
+    phoneCode: '216',
+    flagClassCode: 'tn',
+  },
+  {
+    name: 'Turkey',
+    phoneCode: '90',
+    flagClassCode: 'tr',
+  },
+  {
+    name: 'Turkmenistan',
+    phoneCode: '993',
+    flagClassCode: 'tm',
+  },
+  {
+    name: 'Tuvalu',
+    phoneCode: '688',
+    flagClassCode: 'tv',
+  },
+  {
+    name: 'Uganda',
+    phoneCode: '256',
+    flagClassCode: 'ug',
+  },
+  {
+    name: 'Ukraine',
+    phoneCode: '380',
+    flagClassCode: 'ua',
+  },
+  {
+    name: 'United Arab Emirates',
+    phoneCode: '971',
+    flagClassCode: 'ae',
+  },
+  {
+    name: 'United Kingdom',
+    phoneCode: '44',
+    flagClassCode: 'gb',
+  },
+  {
+    name: 'United States',
+    phoneCode: '1',
+    flagClassCode: 'us',
+  },
+  {
+    name: 'Uruguay',
+    phoneCode: '598',
+    flagClassCode: 'uy',
+  },
+  {
+    name: 'Uzbekistan',
+    phoneCode: '998',
+    flagClassCode: 'uz',
+  },
+  {
+    name: 'Vanuatu',
+    phoneCode: '678',
+    flagClassCode: 'vu',
+  },
+  {
+    name: 'Vatican City',
+    phoneCode: '39-06',
+    flagClassCode: 'va',
+  },
+  {
+    name: 'Venezuela',
+    phoneCode: '58',
+    flagClassCode: 've',
+  },
+  {
+    name: 'Vietnam',
+    phoneCode: '84',
+    flagClassCode: 'vn',
+  },
+  {
+    name: 'Virgin Islands',
+    phoneCode: '1-340',
+    flagClassCode: 'vi',
+  },
+  {
+    name: 'Yemen',
+    phoneCode: '967',
+    flagClassCode: 'ye',
+  },
+  {
+    name: 'Zambia',
+    phoneCode: '260',
+    flagClassCode: 'zm',
+  },
+  {
+    name: 'Zimbabwe',
+    phoneCode: '263',
+    flagClassCode: 'zw',
+  },
+];
